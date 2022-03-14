82c3cee30cd5fd397d3079c3074ecb322f05f5af
Ryo Takeuchi <develop@r09i.me>
2022-03-15 08:39:38 +0900

    :sparkles: Migrate to version catalog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,12 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-
 plugins {
     application
-    kotlin("jvm") version "1.6.10"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 group = "me.r09i.nito"
 version = "0.1.0"
+
 application {
     mainClass.set("me.r09i.nito.app.ApplicationKt")
 
@@ -18,13 +15,10 @@ application {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation(libs.bundles.ktor)
+    implementation(libs.logback.classic)
+    testImplementation(libs.ktor.server.tests.jvm)
+    testImplementation(libs.kotlin.test.junit)
 
     implementation(libs.bundles.koin)
     testImplementation(libs.koin.test)
--- kernel/build.gradle.kts
+++ kernel/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
+    alias(libs.plugins.kotlin)
 }
 
 group = "me.r09i.nito"
 version = "unspecified"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib")
 }
